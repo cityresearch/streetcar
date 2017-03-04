49327a288152ec519de23a4773d29e8e2777a63b
cityresearch <communityresearch@gmail.com>
2017-03-04 12:14:07 -0500

    adding streetcar georectified map

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var handle = document.getElementById('handle'),
     startTop;
 
 var map = L.map('map',{
-      minZoom: 16,
+      minZoom: 8,
       maxZoom: 19
     })
-    .setView([38.048069,-84.503939], 18);
+    .setView([38.220448, -85.73699], 13);
 
 var gBase = new L.Google();
 
 map.addLayer(gBase);
 
-var overlay = L.tileLayer('https://api.mapbox.com/v4/lexhousingstudies.carxus22/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGV4aG91c2luZ3N0dWRpZXMiLCJhIjoiY2lqY2NhZ2VjMDAxZHV1bTBwc2hkbHJ4bCJ9.6eF0ROiV6x6wBDhdDOv2RA', {
-        attribution: '<a href="http://libraries.uky.edu/" target="_blank">University of Kentucky Libraries</a>',
+// var overlay = L.tileLayer('https://api.mapbox.com/v4/lexhousingstudies.carxus22/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGV4aG91c2luZ3N0dWRpZXMiLCJhIjoiY2lqY2NhZ2VjMDAxZHV1bTBwc2hkbHJ4bCJ9.6eF0ROiV6x6wBDhdDOv2RA', {
+    
+ var overlay = L.tileLayer('https://api.mapbox.com/v4/maptimelou.54i84v0m/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwdGltZWxvdSIsImEiOiJjaXYzM3NsbmwwMTZoMm9vYXVva3gxN2ttIn0.obxey61xwB2yWaR5Ca1Psw', {   
+        attribution: '<a href="https://www.ridetarc.org/" target="_blank">Transit Authority of the River City</a>',
         opacity: 0.75,
-        maxNativeZoom: 19,
+       maxNativeZoom: 19,
         maxZoom: 19
       })
       .addTo(map);
